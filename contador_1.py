68e09fbc81871424c2db87b163b3cdd55767432c
aris-osorio <aris.osorio@alumnos.udg.mx>
2020-11-14 03:18:00 -0600

    show dict

--- contador_1.py
+++ contador_1.py
@@ -1,6 +1,10 @@
 import re
 from unidecode import unidecode
 
+def imprimir_tabla(diccionario):
+    for palabra, repeticiones in diccionario.items():
+        print(palabra, '-->', repeticiones)
+
 def contar_palabras(lista):
     diccionario = {}
     for palabra in lista:
@@ -10,7 +14,6 @@ def contar_palabras(lista):
             diccionario[palabra] = 1
     return diccionario
 
-
 def crear_lista(cadena):
     cadena = cadena.replace("\n" , " ")
     cadena = re.sub(r"[^A-Za-z0-9áéíóúüñÁÉÍÓÚÜÑ]+", " ", cadena)
@@ -19,20 +22,21 @@ def crear_lista(cadena):
     lista = cadena.split(" ")
     return lista
 
-
 def obtener_cadena():
     nombre = input("nombre o direccion de libro: ")
     archivo = open(nombre,  encoding="utf8")
     cadena = archivo.read()
     archivo.close()
     return cadena
-
+        
 def main():
-    cadena = obtener_cadena()
-    lista = crear_lista(cadena)
-    diccionario = contar_palabras(lista)
-    print(diccionario)
-
+    try:
+        cadena = obtener_cadena()
+        lista = crear_lista(cadena)
+        diccionario = contar_palabras(lista)
+        imprimir_tabla(diccionario)
+    except:
+        print("Error al procesar archivo")
 
 if __name__ == "__main__":
     main()
